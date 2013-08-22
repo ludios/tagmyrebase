791374c1a52831a1929b11d47d788e98a1e0a1d5
Ivan Kozik <ivan@ludios.org>
2013-08-22 08:07:49 +0000

    Don't add a new tag if the commit already has a tag that matches the format string

--- tagmyrebase.py
+++ tagmyrebase.py
@@ -13,6 +13,7 @@ This allows you to
 
 __version__ = '0.1'
 
+import re
 import sys
 import datetime
 import subprocess
@@ -24,8 +25,23 @@ def call(cmd):
 	return subprocess.check_call(cmd)
 
 
+def get_re_for_format_string(format_string):
+	"""
+	Returns a regular expression for a format string, so that we can check
+	if a tag already exists.
+	"""
+	YMDHMS_RE = r'\d\d\d\d-[01]\d-[0-3]\d_[0-2]\d-[0-5]\d-[0-6]\d'
+	return re.compile(r'\A' + format_string.replace('{YMDHMS}', YMDHMS_RE) + r'\Z')
+
+
+def get_tags_on_commit(commit):
+	stdout = subprocess.check_output(["git", "tag", "--points-at", commit])
+	tags = stdout.replace("\r", "").strip("\n").split("\n")
+	return tags
+
+
 def get_expanded_name(format_string, t):
-	return format_string % dict(YMDHMS=t.strftime('%Y-%m-%d_%H-%M-%S'))
+	return format_string.format(YMDHMS=t.strftime('%Y-%m-%d_%H-%M-%S'))
 
 
 def get_tag_message(upstream_commit):
@@ -93,7 +109,7 @@ def main():
 	2) see in tig/gitk which commits you've previously rebased onto.
 
 	For an of --tag-head, --branch-head, and --tag-upstream, you can use
-	%(YMDHMS)s to insert the current time.
+	{YMDHMS} to insert the current time.
 	""")
 
 	parser.add_argument('-t', '--tag-head', dest='tag_head',
@@ -112,26 +128,35 @@ def main():
 			"--branch-head, or --tag-upstream; see --help")
 		sys.exit(1)
 
-	#branch_name = args.branch_name
-	#if get_commit_or_none(branch_name) == get_commit("HEAD"):
-	#	print >>sys.stderr, "HEAD is already marked as %s" % (branch_name,)
-	#else:
-
 	t = now()
 
 	if args.branch_head:
-		call(["git", "branch", "-f", get_expanded_name(args.branch_head, t)])
+		branch_name = get_expanded_name(args.branch_head, t)
+		if get_commit_or_none(branch_name) == get_commit("HEAD"):
+			print >>sys.stderr, "HEAD is already marked as %s; skipping branch -f." % (branch_name,)
+		else:
+			call(["git", "branch", "-f", branch_name])
 
 	if args.tag_head or args.tag_upstream:
 		upstream_commit = get_upstream_commit()
 
 	if args.tag_upstream:
-		call(["git", "tag", "-a", "--message", "",
-			get_expanded_name(args.tag_upstream, t), upstream_commit])
+		existing_tags_on_upstream = get_tags_on_commit(upstream_commit)
+		if any(get_re_for_format_string(args.tag_upstream).match(tag) for tag in existing_tags_on_upstream):
+			print >>sys.stderr, "Upstream commit %s already has tags " \
+				"%r; not adding another tag." % (upstream_commit, existing_tags_on_upstream)
+		else:
+			call(["git", "tag", "-a", "--message", "",
+				get_expanded_name(args.tag_upstream, t), upstream_commit])
 
 	if args.tag_head:
-		call(["git", "tag", "-a", "--message", get_tag_message(upstream_commit),
-			get_expanded_name(args.tag_head, t)])
+		existing_tags_on_head = get_tags_on_commit("HEAD")
+		if any(get_re_for_format_string(args.tag_head).match(tag) for tag in existing_tags_on_head):
+			print >>sys.stderr, "HEAD already has tags " \
+				"%r; not adding another tag." % (existing_tags_on_head,)
+		else:
+			call(["git", "tag", "-a", "--message", get_tag_message(upstream_commit),
+				get_expanded_name(args.tag_head, t)])
 
 
 if __name__ == '__main__':
