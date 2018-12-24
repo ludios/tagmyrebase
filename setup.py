4e396326c2c053713bc6699ff572cd96669fa041
Ivan Kozik <ivan@ludios.org>
2018-12-24 09:43:52 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+	name="tagmyrebase",
+	version="2.0.0",
+	description="Utility to tag HEAD and the upstream commit after a rebase",
+	scripts=["tagmyrebase"],
+)
